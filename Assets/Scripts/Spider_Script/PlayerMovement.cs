a46fbf39ca6a92570eb8f5db2516418e341391fc
sarah nguyen <107088382+Sarahpixel@users.noreply.github.com>
2023-05-05 20:27:23 +1000

    Update

--- Assets/Scripts/Spider_Script/PlayerMovement.cs
+++ Assets/Scripts/Spider_Script/PlayerMovement.cs
@@ -7,9 +7,6 @@ public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
     private float speed = 5f;
-    //private float jumpingPower = 10f;
-    //private bool isFacingRight = true;
-
     public Animator anim;
 
     [SerializeField] private Rigidbody2D rb;
@@ -20,18 +17,9 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal");
 
+        // sets our float parameters based on if the A D or left and righ keys are pressed
         anim.SetFloat("Speed",Mathf.Abs( horizontal));
-        //if (Input.GetButtonDown("Jump") && IsGrounded())
-        //{
-        //    rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-        //}
-
-        //if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
-        //{
-        //    rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-        //}
-
-        //Flip();
+       
     }
 
     private void FixedUpdate()
@@ -44,14 +32,5 @@ public class PlayerMovement : MonoBehaviour
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 
-    //private void Flip()
-    //{
-    //    if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
-    //    {
-    //        isFacingRight = !isFacingRight;
-    //        Vector3 localScale = transform.localScale;
-    //        localScale.x *= -1f;
-    //        transform.localScale = localScale;
-    //    }
-    //}
+   
 }
